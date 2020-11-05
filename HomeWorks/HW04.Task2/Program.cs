42a0b8feb00d83e6343f73ba4b803299c81cb5c4
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2020-11-05 12:13:52 +0300

    HW04.Task2

---
+++ HomeWorks/HW04.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW04.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task2();
+            
+            static void Task2()
+            {
+                Console.WriteLine("Enter first number:");
+                double firstNumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number:");
+                double secondNumber = double.Parse(Console.ReadLine());
+
+                double result = firstNumber + secondNumber;
+                Console.WriteLine($"Result: {result}");
+            }
+        }
+    }
+}
