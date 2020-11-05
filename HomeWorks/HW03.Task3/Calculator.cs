a5086fa2c4764132c881d14d59159826f22e54fa
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2020-11-05 09:44:13 +0300

    HW03.Task3

---
+++ HomeWorks/HW03.Task3/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW03.Task3
+{
+    class Calculator
+    {
+        public static void AddingTwoNumbers()
+        {
+            Console.WriteLine("Adding two numbers");
+            double firstNumber = FirstNumber();
+            double secondNumber = SecondNumber();
+            double result = firstNumber + secondNumber;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        public static void SubtractingTwoNumbers()
+        {
+            Console.WriteLine("Subtracting two numbers");
+            double firstNumber = FirstNumber();
+            double secondNumber = SecondNumber();
+            double result = firstNumber - secondNumber;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        public static void MultiplicationOfTwoNumbers()
+        {
+            Console.WriteLine("Multiplication of two numbers");
+            double firstNumber = FirstNumber();
+            double secondNumber = SecondNumber();
+            double result = firstNumber * secondNumber;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        public static void DivisionOfTwoNumbers()
+        {
+            Console.WriteLine("Division of two numbers");
+            double firstNumber = FirstNumber();
+            double secondNumber = SecondNumber();
+            double result = firstNumber / secondNumber;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        public static void RemainderOfDivision()
+        {
+            Console.WriteLine("Remainder of the division");
+            double firstNumber = FirstNumber();
+            double secondNumber = SecondNumber();
+            double result = firstNumber % secondNumber;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("Area of a circle");
+            double firstNumber = FirstNumber();
+            double result = Math.PI * Math.Pow(firstNumber, 2);
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private static double FirstNumber()
+        {
+            Console.WriteLine("Enter the first number:");
+            double firstNumber = double.Parse(Console.ReadLine());
+            return firstNumber;
+        }
+
+        private static double SecondNumber()
+        {
+            Console.WriteLine("Enter the second number:");
+            double secondNumber = double.Parse(Console.ReadLine());
+            return secondNumber;
+        }
+    }
+}
---
+++ HomeWorks/HW03.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW03.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.AddingTwoNumbers();
+            Calculator.SubtractingTwoNumbers();
+            Calculator.MultiplicationOfTwoNumbers();
+            Calculator.DivisionOfTwoNumbers();
+            Calculator.RemainderOfDivision();
+            Calculator.AreaOfCircle();
+        }
+    }
+}
