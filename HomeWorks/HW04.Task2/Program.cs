938307cdea956ab1abc03ac7ec68b499f9244b9b
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2020-11-05 12:25:18 +0300

    HW04.Task3

--- HomeWorks/HW04.Task2/Program.cs
+++ HomeWorks/HW04.Task2/Program.cs
@@ -6,7 +6,8 @@ namespace HW04.Task2
     {
         static void Main(string[] args)
         {
-            Task2();
+            //Task2();
+            Task3();
             
             static void Task2()
             {
@@ -18,6 +19,25 @@ namespace HW04.Task2
 
                 double result = firstNumber + secondNumber;
                 Console.WriteLine($"Result: {result}");
+            }
+
+            static void Task3()
+            {
+                Console.WriteLine("Enter first number:");
+                double firstNumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number:");
+                double secondNumber = double.Parse(Console.ReadLine());
+
+                double result = firstNumber + secondNumber;
+
+                Console.WriteLine("Enter the result of adding these numbers:");
+                double userResult = double.Parse(Console.ReadLine());
+
+                if (userResult == result)
+                    Console.WriteLine("Correctly!");
+                else
+                    Console.WriteLine("Wrong!");
             }
         }
     }
