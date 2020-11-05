697627bb12a0082ac2d91eb6027c2ea5c879b329
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2020-11-05 13:11:02 +0300

    HW04.Task5

--- HomeWorks/HW04.Task2/Program.cs
+++ HomeWorks/HW04.Task2/Program.cs
@@ -8,7 +8,8 @@ namespace HW04.Task2
         {
             //Task2();
             //Task3();
-            Task4();
+            //Task4();
+            Task5();
             
             static void Task2()
             {
@@ -52,6 +53,39 @@ namespace HW04.Task2
                 double result = firstNumber + secondNumber;
 
                 Console.WriteLine("Enter the result of adding these numbers:");
+                double userResult = double.Parse(Console.ReadLine());
+
+                if (userResult == result)
+                    Console.WriteLine("Correctly!");
+                else
+                {
+                    Console.WriteLine("Wrong!");
+
+                    if (userResult < result)
+                        Console.WriteLine("There must be more!");
+                    else
+                        Console.WriteLine("There must be less!");
+                }
+            }
+
+            static void Task5()
+            {
+                Console.WriteLine("Enter first number:");
+                double firstNumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number:");
+                double secondNumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter + or - :");
+                string userChoice = Console.ReadLine();
+
+                double result;
+                if (userChoice == "+")
+                    result = firstNumber + secondNumber;
+                else
+                    result = firstNumber - secondNumber;
+
+                Console.WriteLine("Enter the result:");
                 double userResult = double.Parse(Console.ReadLine());
 
                 if (userResult == result)
