f6d5f81787a6ccdee152f6f0709f06b287c99703
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-12-06 20:31:39 +0300

    HW11.Task1

---
+++ HomeWorks/HW11.Task1/Persons/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW11.Task1.Persons
+{
+    class Teacher : Person
+    {
+        private string _subject;
+
+        public override void Greeting()
+        {
+            base.Greeting();
+            Console.WriteLine("I am a teacher.");
+        }
+
+        public void Explain()
+        {
+            Console.WriteLine("Explanation  begins.");
+        }
+    }
+}
