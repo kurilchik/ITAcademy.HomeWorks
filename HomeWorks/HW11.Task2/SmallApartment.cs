c52a568ec966e196fa41555454f78fb3dafcb8a3
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2020-12-07 09:09:51 +0300

    HW11.Task2

---
+++ HomeWorks/HW11.Task2/SmallApartment.cs
@@ -0,0 +1,10 @@
+﻿namespace HW11.Task2
+{
+    class SmallApartment : House
+    {
+        public SmallApartment(string doorColor) : base(doorColor)
+        {
+            _area = 50;
+        }
+    }
+}
