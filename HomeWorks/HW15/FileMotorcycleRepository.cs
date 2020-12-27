dc107b785b1c8245b5cd9961355201e5c14f1551
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-12-27 22:59:22 +0300

    HW15

---
+++ HomeWorks/HW15/FileMotorcycleRepository.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW15
+{
+    class FileMotorcycleRepository : IMotorcycle
+    {
+        public void CreateMotorcycle(Motorcycle motorcycle)
+        {
+            string savePath = $"C:\\Users\\DELL\\Documents\\motorcycle{motorcycle.Id}.json";
+
+            File.WriteAllText(savePath, JsonConvert.SerializeObject(motorcycle));
+        }
+
+        public void DeleteMotorcycle(int id)
+        {
+            string savePath = $"C:\\Users\\DELL\\Documents\\motorcycle{id}.json";
+
+            FileInfo userSave = new FileInfo(savePath);
+            if (userSave.Exists)
+            {
+                userSave.Delete();
+            }
+            else
+                Console.WriteLine("File not found!");
+        }
+
+        public Motorcycle GetMotorcycleByID(int id)
+        {
+            string savePath = $"C:\\Users\\DELL\\Documents\\motorcycle{id}.json";
+
+            return JsonConvert.DeserializeObject<Motorcycle>(File.ReadAllText(savePath));
+        }
+
+        public List<Motorcycle> GetMotorcycles()
+        {
+            List<Motorcycle> motorcycles = new List<Motorcycle>();
+
+            string savePath = $"C:\\Users\\DELL\\Documents\\";
+
+            DirectoryInfo saveFolder = new DirectoryInfo(savePath);
+            FileInfo[] saveFiles = saveFolder.GetFiles();
+
+            for (int i = 0; i < saveFiles.Length; i++)
+            {
+                if (saveFiles[i].Extension == ".json")
+                {
+                    motorcycles.Add(JsonConvert.DeserializeObject<Motorcycle>(File.ReadAllText(saveFiles[i].FullName)));
+                }
+            }
+
+            return motorcycles;
+        }
+
+        public void UpdateMotorcycle(Motorcycle motorcycle)
+        {
+            DeleteMotorcycle(motorcycle.Id);
+
+            string savePath = $"C:\\Users\\DELL\\Documents\\motorcycle{motorcycle.Id}.json";
+
+            File.WriteAllText(savePath, JsonConvert.SerializeObject(motorcycle));
+        }
+    }
+}
---
+++ HomeWorks/HW15/ListMotorcycleRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace HW15
+{
+    class ListMotorcycleRepository : IMotorcycle
+    {
+        List<Motorcycle> _motorcycles = new List<Motorcycle>();
+
+        public void CreateMotorcycle(Motorcycle motorcycle)
+        {
+            _motorcycles.Add(motorcycle);
+        }
+
+        public void DeleteMotorcycle(int id)
+        {
+            _motorcycles.Remove(_motorcycles.Find(x => x.Id == id));
+        }
+
+        public Motorcycle GetMotorcycleByID(int id)
+        {
+            return _motorcycles.Find(x => x.Id == id);
+        }
+
+        public List<Motorcycle> GetMotorcycles()
+        {
+            return _motorcycles;
+        }
+
+        public void UpdateMotorcycle(Motorcycle motorcycle)
+        {
+            _motorcycles.Remove(_motorcycles.Find(x => x.Id == motorcycle.Id));
+            _motorcycles.Add(motorcycle);
+        }
+    }
+}
